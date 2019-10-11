f35386112b6e067907d6390d0de7baa4d48e6b6e
admin <admin@admins-MacBook-Pro.local>
2019-10-11 08:16:02 -0700

    questions completed and file structure set up

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,14 @@
+
+
+
+const initialState = {
+     smurfData: []
+}
+
+const reducers = (state = initialState, action) => {
+     switch(action.type){
+          default:
+               return state 
+     }
+}
+export default reducers
