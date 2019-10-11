546ed2a1a17f9e786d8fb480cc60482ca9b58376
admin <admin@admins-MacBook-Pro.local>
2019-10-11 08:45:22 -0700

    successful axios call

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Smurfs(props){
+
+     console.log(props)
+
+     return(
+          <div>Hello I am the child smurf component</div>
+     )
+}
+
+export default Smurfs
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,12 +1,32 @@
-
+import {SMURF_START, SMURF_SUCCESS, SMURF_FAILURE} from '../actions/'
 
 
 const initialState = {
-     smurfData: []
+     smurfs: [],
+     errors: '',
+     isFetching: false
 }
 
 const reducers = (state = initialState, action) => {
      switch(action.type){
+          case SMURF_START: 
+               return{
+                    ...state,
+                    isFetching: true
+               }
+          case SMURF_SUCCESS:
+               return{
+                    ...state,
+                    errors: '',
+                    isFetching: false, 
+                    smurfs: action.payload
+               }
+          case SMURF_FAILURE:
+               return{
+                    ...state, 
+                    errors: "Uh oh! You didn't fetch your smurf data",
+                    isFetching: false
+               }
           default:
                return state 
      }
